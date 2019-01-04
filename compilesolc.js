f368239cd6f14dd6dea6352e9b6f4bd4625b87f9
Mudit Gupta <guptamudit@ymail.com>
2019-01-04 13:16:31 +0530

    Added sample

---
+++ compilesolc.js
@@ -0,0 +1,10 @@
+var solc = require('solc');
+var fs = require('fs');
+
+fs.readFile('x.txt', 'utf8', function(err, data) {
+    var output = JSON.parse(solc.compile(JSON.stringify(data)));
+	console.log(output);
+	// var output = JSON.parse(solc.compileStandard(JSON.stringify(data)));
+	// console.log(output);
+});
+
