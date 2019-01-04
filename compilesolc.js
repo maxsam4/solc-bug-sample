6330827ee0066049166866c6c8cca6b0ca517ddb
Mudit Gupta <guptamudit@ymail.com>
2019-01-04 13:27:03 +0530

    indentation

--- compilesolc.js
+++ compilesolc.js
@@ -3,8 +3,8 @@ var fs = require('fs');
 
 fs.readFile('x.txt', 'utf8', function(err, data) {
     var output = JSON.parse(solc.compile(JSON.stringify(data)));
-	console.log(output);
-	// var output = JSON.parse(solc.compileStandard(JSON.stringify(data)));
-	// console.log(output);
+    console.log(output);
+    // var output = JSON.parse(solc.compileStandard(JSON.stringify(data)));
+    // console.log(output);
 });
 
